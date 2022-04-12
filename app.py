1aab52011dd9a2b3c1fd4942f2dea90554469515
Pranjal Bhardwaj <decimatormind@gmail.com>
2022-04-12 14:21:42 +0530

    Add cloudinary functions

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ from flask import Flask,jsonify,request,make_response
 from flask_sqlalchemy import SQLAlchemy
 import jwt
 from werkzeug.security import generate_password_hash, check_password_hash
+import cloudinary.uploader
 
 app = Flask(__name__)
+
+# yorec77612@bamibi.com
 
 secret_key = "GlueLabs"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234567@localhost/ToDoAPI_GL'
@@ -28,7 +31,7 @@ class Tasks(db.Model):
     last_date = db.Column(db.Date)
     user_order = db.Column(db.Integer,nullable = False)
     last_update = db.Column(db.Date,nullable = False)
-    deleted = db.Column(db.Boolean,nullable = False)
+    deleted = db.Column(db.Date,nullable = True)
 
 
 db.create_all()
@@ -39,6 +42,11 @@ def getUserOrder(userId):
     for i in tasks:
         order += 1
     return order
+
+cloudinary.config(
+  cloud_name = "dxr7lcjmd",
+  api_key = "643921477167134",
+  api_secret = "z9WyxtR6Io6ACEQsA_yylzxfk68")
 
 # task = Tasks(id = 1,task = "Test Task",user_id = 1,completed = False)
 # db.session.add(task)
@@ -65,12 +73,15 @@ def home():
 
 @app.route("/register",methods = ["POST"])
 def register():
-    data = request.get_json()
-    password = generate_password_hash(data["password"])
-    new_user = Users(name = data["name"],email = data["email"],password = password)
+    user_name = request.form.get('user_name')
+    user_email = request.form.get('user_email')
+    password = generate_password_hash(request.form.get('password'))
+    new_user = Users(name = user_name,email = user_email,password = password)
     db.session.add(new_user)
     try:
         db.session.commit()
+        data = request.files["display_picture"]
+        cloudinary.uploader.upload(data,public_id = user_email)
         return make_response(jsonify({"status" : "Successfully Added to Database"}),201)
     except:
         db.session.rollback()
@@ -138,7 +149,7 @@ def tasks():
                     return make_response(jsonify({'error' : 'Access Not Allowed'}),401)
                 user_order = getUserOrder(userId)
                 date_today = datetime.date.today()
-                task = Tasks(title = json['title'],description = json['description'],user_id = json['user_id'],completed = json['completed'],initial_date = date_today,last_date = date_today+datetime.timedelta(days=json["number_of_days"]),user_order = user_order,last_update = date_today,deleted = False)
+                task = Tasks(title = json['title'],description = json['description'],user_id = json['user_id'],completed = json['completed'],initial_date = date_today,last_date = date_today+datetime.timedelta(days=json["number_of_days"]),user_order = user_order,last_update = date_today,deleted = None)
                 db.session.add(task)
                 try:
                     db.session.commit()
@@ -185,9 +196,11 @@ def tasks():
                 user = json['user_id']
                 taskid = json['id']
                 temp = Tasks.query.filter_by(user_id = user).filter_by(id = taskid).first()
-                temp.deleted = True
+                temp.deleted = datetime.date.today()
+                temp.user_order = 0
                 try:
                     db.session.commit()
+                    reorder()
                     return make_response(jsonify({"status":"Successfully Deleted from Database"}),200)
                 except:
                     db.session.rollback()
@@ -232,27 +245,26 @@ def reorder():
     try:
         payload = jwt.decode(token, secret_key, algorithms=["HS256"])
         userId = payload['public_id']
-        if(request.method == "GET"):
-            tasks = Tasks.query.filter_by(user_id = userId).order_by(Tasks.last_date.asc())
-            if(tasks == None):
-                return make_response(jsonify({'status': 'No Task Found'}),200)
-            else:
-                new_index = 0
-                result = {}
-                for i in tasks:
-                    if(i.deleted == False):
-                        i.user_order = new_index
-                        new_index += 1
-                        result[i.id] = {"title":i.title,"description": i.description,"user_id":i.user_id,"completed":i.completed,"last_date":i.last_date,"last_update":i.last_update,"user_order": i.user_order}
-                try:
-                    db.session.commit()
-                    return make_response(jsonify({"status":"Successfully Reordered Items"}),200)
-                except:
-                    db.session.rollback()
-                    return make_response(jsonify({"status":"Error in Writing to Database"}),400)
-                finally:
-                    db.session.close()
-                    return make_response(jsonify(result),200)
+        tasks = Tasks.query.filter_by(user_id = userId).order_by(Tasks.last_date.asc())
+        if(tasks == None):
+            return make_response(jsonify({'status': 'No Task Found'}),200)
+        else:
+            new_index = 0
+            result = {}
+            for i in tasks:
+                if(i.deleted == False):
+                    i.user_order = new_index
+                    new_index += 1
+                    result[i.id] = {"title":i.title,"description": i.description,"user_id":i.user_id,"completed":i.completed,"last_date":i.last_date,"last_update":i.last_update,"user_order": i.user_order}
+            try:
+                db.session.commit()
+                return make_response(jsonify({"status":"Successfully Reordered Items"}),200)
+            except:
+                db.session.rollback()
+                return make_response(jsonify({"status":"Error in Writing to Database"}),400)
+            finally:
+                db.session.close()
+                return make_response(jsonify(result),200)
     except:
         return make_response(jsonify({'status' : 'Token Expired'}),400)
 
