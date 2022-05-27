a87cf473b0744d14bd624f5bc6717ee6647cbe1e
Pranjal Bhardwaj <decimatormind@gmail.com>
2022-05-27 17:35:52 +0530

    Update users.py

--- resources/users.py
+++ resources/users.py
@@ -34,7 +34,7 @@ class User_Login(Resource):
         user_email = request.form.get('user_email')
         user_password = request.form.get('user_password')
         returned_user = DbUser.find_by_username(user_email)
-        return jsonify({"status":returned_user})
+        return jsonify({"status":returned_user.password})
         # if(check_password_hash(returned_user.password, user_password)):
         #     token = jwt.encode({'public_id' : returned_user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=60)}, secret_key, "HS256")
         #     return make_response(jsonify({'token': token}), 201)
