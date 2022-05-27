785c3dfc351e1c09996fa02c280307cfbdedfc2d
Pranjal Bhardwaj <decimatormind@gmail.com>
2022-05-27 15:58:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ api = Api(app)
 
 secret_key = "GlueLabs"
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234567@localhost/ToDoAPI_GL'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zwkyozttqbjolj:025b9d9677e8ce2a41782fca237d09ac2723ca9c2787faa9257641a30a3465e5@ec2-34-230-153-41.compute-1.amazonaws.com:5432/d3fls186trme57'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://zwkyozttqbjolj:025b9d9677e8ce2a41782fca237d09ac2723ca9c2787faa9257641a30a3465e5@ec2-34-230-153-41.compute-1.amazonaws.com:5432/d3fls186trme57'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
