ed3939f82056816b3c5a85d5e06807b147d99843
Pranjal Bhardwaj <decimatormind@gmail.com>
2022-05-30 10:21:00 +0530

    Update users.py

--- resources/users.py
+++ resources/users.py
@@ -35,7 +35,8 @@ class User_Login(Resource):
         user_password = request.form.get('user_password')
         returned_user = DbUser.find_by_user_email(user_email)
         if(check_password_hash(returned_user.password, user_password)):
-            token = jwt.encode({'public_id' : returned_user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=60)}, secret_key, "HS256")
-            return make_response(jsonify({'token': str(token)}), 201)
+            # token = jwt.encode({'public_id' : returned_user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=60)}, secret_key, "HS256")
+            return make_repsonse(jsonify({"status" : returned_user.email}))
+            # return make_response(jsonify({'token': str(token)}), 201)
         else:
             return make_response(jsonify({'status': 'Wrong password entered'}),400)
